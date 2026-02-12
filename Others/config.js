5a1a9f6fb7c9708ef025d35de128d63b41122d8a
nxthxn <103566265+nxthxndev@users.noreply.github.com>
2026-02-12 14:21:36 +0100

    Update Firebase configuration in config.js
    Updated Firebase configuration with new project details.

--- Others/config.js
+++ Others/config.js
@@ -6,16 +6,27 @@
 // 3. Ajoutez une application Web
 // 4. Copiez la configuration fournie ici
 // 5. Activez "Realtime Database" dans la console Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "VOTRE_API_KEY",
-    authDomain: "VOTRE_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://VOTRE_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "VOTRE_PROJECT_ID",
-    storageBucket: "VOTRE_PROJECT_ID.appspot.com",
-    messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
-    appId: "VOTRE_APP_ID"
+  apiKey: "AIzaSyB2Ox6EjEnq3jkpepBR1SBNqiY6m2Wh3LM",
+  authDomain: "ne-battle-royale.firebaseapp.com",
+  projectId: "ne-battle-royale",
+  storageBucket: "ne-battle-royale.firebasestorage.app",
+  messagingSenderId: "612745279766",
+  appId: "1:612745279766:web:5a086ae06f40c16994f837",
+  measurementId: "G-H0BRJYYFQD"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialisation de Firebase
 firebase.initializeApp(firebaseConfig);
